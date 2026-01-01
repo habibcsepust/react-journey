b523d0b632efc3cc1a3599aae1aa7dcf9e3db226
Habibur <habibur.it@nblbd.com>
2026-01-01 18:33:49 +0600

    Json added

---
+++ src/Component/JsonHandle/BasicInfo.jsx
@@ -0,0 +1,35 @@
+
+const users = [
+    {
+        "Name" : "Habibur Rahman",
+        "Email" : "habibcsepust@gmail.com",
+        phones : [
+            {
+                "Mobile":"01302526321",
+                "Office":"85426320"
+            }
+        ],
+        "Gender":"Male"
+    },
+    {
+        "Name" : "Haidar Ali",
+        "Email" : "haidar@gmail.com",
+        phones : [
+            {
+                "Mobile":"01752526321",
+                "Office":"242014320"
+            }
+        ],
+        "Gender":"Female"
+    },
+    {
+        "Name" : "Yameen Sajid",
+        "Email" : "yameensajid@gmail.com",
+        phones : [
+            {
+                "Mobile":"01802522523",
+                "Office":"32526320"
+            }
+        ]
+    }
+]
